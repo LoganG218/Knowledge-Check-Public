68fbeca57b3cd6e6e698605260c0583fb37113dd
LoganG218 <125411469+LoganG218@users.noreply.github.com>
2023-02-14 17:49:35 -0500

    Update Calculator.cs

--- KnowledgeCheck1_Calculator/Calculator.cs
+++ KnowledgeCheck1_Calculator/Calculator.cs
@@ -18,9 +18,9 @@ namespace KnowledgeCheck1_Calculator
             return first - second;
         }
 
-        public int Multiply(int first, int second)
+       public int Multiply(int first, int second)
         {
-            throw new NotImplementedException();
+            return first * second;
         }
 
         public double Divide(double first, double second)
